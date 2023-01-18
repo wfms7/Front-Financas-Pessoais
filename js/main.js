d3947d804b32e223aafa6967ec48a42740aa2cac
Walter Fernando Moraes Santos <wfms7@hotmail.com>
2023-01-18 16:40:29 -0300

    add javascript getdata

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 import abrirNavBAr from "./nav-menu.js";
 
 const btnNavMenu = document.getElementById('menu-nav');
+const today =document.querySelector('[data-today]');
 
+const timeElapsed  = Date.now();
+const todayresult = new Date(timeElapsed);
+
+
+today.innerHTML = todayresult.toLocaleDateString() 
 
 btnNavMenu.addEventListener('click', ()=>{
     abrirNavBAr();
 })
+
+
+
