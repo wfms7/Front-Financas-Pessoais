3e38f1b034650974f9d3676f337a8daecd7c1659
Walter Fernando Moraes Santos <wfms7@hotmail.com>
2023-01-20 17:10:20 -0300

    add paginas

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
+
 import abrirNavBAr from "./nav-menu.js";
 
 const btnNavMenu = document.getElementById('menu-nav');
 const today =document.querySelector('[data-today]');
 
-const timeElapsed  = Date.now();
-const todayresult = new Date(timeElapsed);
 
 
-today.innerHTML = todayresult.toLocaleDateString() 
+
+function todayNow(){
+    
+    if (today != null){
+        const timeElapsed  = Date.now();
+        const todayresult = new Date(timeElapsed);
+        today.innerHTML = todayresult.toLocaleDateString() 
+    }
+    
+
+}
+todayNow();
+
+
 
 btnNavMenu.addEventListener('click', ()=>{
     abrirNavBAr();
 })
-
-
-
---
+++ js/menu.js
@@ -0,0 +1,66 @@
+const headerMenu = document.querySelector('[data-headerMenu]');
+const headerNav = document.querySelector('[data-headerNav]');
+const url=  window.location.pathname;
+
+
+
+
+
+
+
+
+ function criarMenu(){
+   let urlPage =''
+   if(url != '/' && url != '/index.html')
+   {
+      urlPage ='..'
+   }
+
+    headerMenu.innerHTML = `
+         <div class="header__container">
+        <button class="header__menu" id="menu-nav"><img class="header__menu-imagem" src="../img/menu.svg" alt="menu"> </button>
+        <img class="header__imagem-logo" src="../img/Logo.png" alt="Logo Finança Pessoal">
+        <div class="header__user">
+           <p class="header__user-acronym">WF</p>       
+        </div>
+        </div>
+    
+    `
+    headerNav.innerHTML=`
+    <div class="nav-menu__container" data-navmenu>
+    <ul class="nav-menu__lista">
+        <li class="nav-menu__item "><a href="${urlPage}/index.html" class="nav-menu__link" data-url data-url>Dashboard</a></li>
+        <li class="nav-menu__item"><a href="${urlPage}/page/perfil.html" class="nav-menu__link" data-url>Perfil</a></li>
+        <li class="nav-menu__item"><a href="${urlPage}/page/despesas.html" class="nav-menu__link" data-url>Despesas</a></li>
+        <li class="nav-menu__item"><a href="${urlPage}/page/cartaocredito.html" class="nav-menu__link"data-url>Cartão de Credito</a></li>
+        <li class="nav-menu__item"><a href="${urlPage}/page/renda.html" class="nav-menu__link" data-url>Renda</a></li>
+    </ul>
+    `
+   
+ }
+
+ criarMenu();
+
+ const navAtiva = document.querySelectorAll('[data-url]');
+ 
+
+ function marcarAtiva(){
+
+   navAtiva.forEach(nav  =>{
+     if(url != '/' && url != '/index.html')
+     {
+      if( nav.href.includes(url) ){
+        nav.classList.add('nav-menu__link--ativo');
+        nav.parentElement.classList.add('nav-menu__item--ativo');
+
+      }
+     }
+     else{
+      navAtiva[0].classList.add('nav-menu__link--ativo');
+      navAtiva[0].parentElement.classList.add('nav-menu__item--ativo');
+     }
+     
+   } )
+ }
+
+ marcarAtiva();
